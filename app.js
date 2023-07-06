031294c526aa2d74fbd94e975d10c21f5c153366
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-07-06 10:43:48 -0700

    update JS selectors from IDs to classes
    Requrie this update due to CSS selector priority.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const myButton = document.querySelector("#myButton")
-const ratingCard = document.querySelector("#ratingCard")
-const submissionCard = document.querySelector("#submissionCard")
+const ratingCard = document.querySelector(".ratingCard")
+const submissionCard = document.querySelector(".submissionCard")
 
 myButton.addEventListener("click", () => {
   ratingCard.classList.toggle("hide_display")
