e77647e89f7419dbc89a0d315c51ea6f7cff12b2
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-07-06 09:43:15 -0700

    add JS selectors and toggle hide_display class

---
+++ app.js
@@ -0,0 +1,8 @@
+const myButton = document.querySelector("#myButton")
+const ratingCard = document.querySelector("#ratingCard")
+const submissionCard = document.querySelector("#submissionCard")
+
+myButton.addEventListener("click", () => {
+  ratingCard.classList.toggle("hide_display")
+  submissionCard.classList.toggle("hide_display")
+})
