3307cce810100ca17253e7b5ce146a7c05dfda0e
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-07-06 09:12:09 -0700

    add reactivity to rating numbers
    I suspect there is an easier and more concise way to write JS.

---
+++ app.js
@@ -0,0 +1,106 @@
+const selected_item_1 = document.querySelector("#selected_item_1")
+const selected_item_2 = document.querySelector("#selected_item_2")
+const selected_item_3 = document.querySelector("#selected_item_3")
+const selected_item_4 = document.querySelector("#selected_item_4")
+const selected_item_5 = document.querySelector("#selected_item_5")
+
+selected_item_1.addEventListener("click", function (event) {
+  selected_item_1.classList.toggle("active")
+  selected_item_1.classList.toggle("item")
+  selected_item_2.classList.remove("active")
+  selected_item_3.classList.remove("active")
+  selected_item_4.classList.remove("active")
+  selected_item_5.classList.remove("active")
+  if (!selected_item_2.classList.contains("item")) {
+    selected_item_2.classList.add("item")
+  }
+  if (!selected_item_3.classList.contains("item")) {
+    selected_item_3.classList.add("item")
+  }
+  if (!selected_item_4.classList.contains("item")) {
+    selected_item_4.classList.add("item")
+  }
+  if (!selected_item_5.classList.contains("item")) {
+    selected_item_5.classList.add("item")
+  }
+})
+selected_item_2.addEventListener("click", function (event) {
+  selected_item_2.classList.toggle("active")
+  selected_item_2.classList.toggle("item")
+  selected_item_1.classList.remove("active")
+  selected_item_3.classList.remove("active")
+  selected_item_4.classList.remove("active")
+  selected_item_5.classList.remove("active")
+  if (!selected_item_1.classList.contains("item")) {
+    selected_item_1.classList.add("item")
+  }
+  if (!selected_item_3.classList.contains("item")) {
+    selected_item_3.classList.add("item")
+  }
+  if (!selected_item_4.classList.contains("item")) {
+    selected_item_4.classList.add("item")
+  }
+  if (!selected_item_5.classList.contains("item")) {
+    selected_item_5.classList.add("item")
+  }
+})
+selected_item_3.addEventListener("click", function (event) {
+  selected_item_3.classList.toggle("active")
+  selected_item_3.classList.toggle("item")
+  selected_item_2.classList.remove("active")
+  selected_item_1.classList.remove("active")
+  selected_item_4.classList.remove("active")
+  selected_item_5.classList.remove("active")
+  if (!selected_item_2.classList.contains("item")) {
+    selected_item_2.classList.add("item")
+  }
+  if (!selected_item_1.classList.contains("item")) {
+    selected_item_1.classList.add("item")
+  }
+  if (!selected_item_4.classList.contains("item")) {
+    selected_item_4.classList.add("item")
+  }
+  if (!selected_item_5.classList.contains("item")) {
+    selected_item_5.classList.add("item")
+  }
+})
+selected_item_4.addEventListener("click", function (event) {
+  selected_item_4.classList.toggle("active")
+  selected_item_4.classList.toggle("item")
+  selected_item_2.classList.remove("active")
+  selected_item_3.classList.remove("active")
+  selected_item_1.classList.remove("active")
+  selected_item_5.classList.remove("active")
+  if (!selected_item_2.classList.contains("item")) {
+    selected_item_2.classList.add("item")
+  }
+  if (!selected_item_3.classList.contains("item")) {
+    selected_item_3.classList.add("item")
+  }
+  if (!selected_item_1.classList.contains("item")) {
+    selected_item_1.classList.add("item")
+  }
+  if (!selected_item_5.classList.contains("item")) {
+    selected_item_5.classList.add("item")
+  }
+})
+selected_item_5.addEventListener("click", function (event) {
+  selected_item_5.classList.toggle("active")
+  selected_item_5.classList.toggle("item")
+  selected_item_2.classList.remove("active")
+  selected_item_3.classList.remove("active")
+  selected_item_4.classList.remove("active")
+  selected_item_1.classList.remove("active")
+  if (!selected_item_2.classList.contains("item")) {
+    selected_item_2.classList.add("item")
+  }
+  if (!selected_item_3.classList.contains("item")) {
+    selected_item_3.classList.add("item")
+  }
+  if (!selected_item_4.classList.contains("item")) {
+    selected_item_4.classList.add("item")
+  }
+  if (!selected_item_1.classList.contains("item")) {
+    selected_item_1.classList.add("item")
+  }
+})
