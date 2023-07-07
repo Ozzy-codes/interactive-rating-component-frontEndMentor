b9f3476ce1d7d04a266430e5da01e435dc6e355d
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-07-07 11:27:29 -0700

    refactor JS classList operators

--- app.js
+++ app.js
@@ -8,107 +8,26 @@ const ratingCard = document.querySelector(".ratingCard")
 const submissionCard = document.querySelector(".submissionCard")
 const score_options = document.querySelector("#score_options")
 const selected_score = document.querySelector("#selected_score")
+const selected_item_array = [
+  selected_item_1,
+  selected_item_2,
+  selected_item_3,
+  selected_item_4,
+  selected_item_5
+]
 
-selected_item_1.addEventListener("click", function (event) {
-  selected_item_1.classList.toggle("active")
-  selected_item_1.classList.toggle("item")
-  selected_item_2.classList.remove("active")
-  selected_item_3.classList.remove("active")
-  selected_item_4.classList.remove("active")
-  selected_item_5.classList.remove("active")
-  if (!selected_item_2.classList.contains("item")) {
-    selected_item_2.classList.add("item")
-  }
-  if (!selected_item_3.classList.contains("item")) {
-    selected_item_3.classList.add("item")
-  }
-  if (!selected_item_4.classList.contains("item")) {
-    selected_item_4.classList.add("item")
-  }
-  if (!selected_item_5.classList.contains("item")) {
-    selected_item_5.classList.add("item")
-  }
-})
-selected_item_2.addEventListener("click", function (event) {
-  selected_item_2.classList.toggle("active")
-  selected_item_2.classList.toggle("item")
-  selected_item_1.classList.remove("active")
-  selected_item_3.classList.remove("active")
-  selected_item_4.classList.remove("active")
-  selected_item_5.classList.remove("active")
-  if (!selected_item_1.classList.contains("item")) {
-    selected_item_1.classList.add("item")
-  }
-  if (!selected_item_3.classList.contains("item")) {
-    selected_item_3.classList.add("item")
-  }
-  if (!selected_item_4.classList.contains("item")) {
-    selected_item_4.classList.add("item")
-  }
-  if (!selected_item_5.classList.contains("item")) {
-    selected_item_5.classList.add("item")
-  }
-})
-selected_item_3.addEventListener("click", function (event) {
-  selected_item_3.classList.toggle("active")
-  selected_item_3.classList.toggle("item")
-  selected_item_2.classList.remove("active")
-  selected_item_1.classList.remove("active")
-  selected_item_4.classList.remove("active")
-  selected_item_5.classList.remove("active")
-  if (!selected_item_2.classList.contains("item")) {
-    selected_item_2.classList.add("item")
-  }
-  if (!selected_item_1.classList.contains("item")) {
-    selected_item_1.classList.add("item")
-  }
-  if (!selected_item_4.classList.contains("item")) {
-    selected_item_4.classList.add("item")
-  }
-  if (!selected_item_5.classList.contains("item")) {
-    selected_item_5.classList.add("item")
-  }
-})
-selected_item_4.addEventListener("click", function (event) {
-  selected_item_4.classList.toggle("active")
-  selected_item_4.classList.toggle("item")
-  selected_item_2.classList.remove("active")
-  selected_item_3.classList.remove("active")
-  selected_item_1.classList.remove("active")
-  selected_item_5.classList.remove("active")
-  if (!selected_item_2.classList.contains("item")) {
-    selected_item_2.classList.add("item")
-  }
-  if (!selected_item_3.classList.contains("item")) {
-    selected_item_3.classList.add("item")
-  }
-  if (!selected_item_1.classList.contains("item")) {
-    selected_item_1.classList.add("item")
-  }
-  if (!selected_item_5.classList.contains("item")) {
-    selected_item_5.classList.add("item")
-  }
-})
-selected_item_5.addEventListener("click", function (event) {
-  selected_item_5.classList.toggle("active")
-  selected_item_5.classList.toggle("item")
-  selected_item_2.classList.remove("active")
-  selected_item_3.classList.remove("active")
-  selected_item_4.classList.remove("active")
-  selected_item_1.classList.remove("active")
-  if (!selected_item_2.classList.contains("item")) {
-    selected_item_2.classList.add("item")
-  }
-  if (!selected_item_3.classList.contains("item")) {
-    selected_item_3.classList.add("item")
-  }
-  if (!selected_item_4.classList.contains("item")) {
-    selected_item_4.classList.add("item")
-  }
-  if (!selected_item_1.classList.contains("item")) {
-    selected_item_1.classList.add("item")
-  }
-})
+for (let index = 0; index < selected_item_array.length; index++) {
+  selected_item_array[index].addEventListener("click", function (event) {
+    for (let index = 0; index < score_options.children.length; index++) {
+      score_options.children[index].classList.remove("active")
+      if (!score_options.children[index].classList.contains("item")) {
+        score_options.children[index].classList.add("item")
+      }
+    }
+    selected_item_array[index].classList.toggle("active")
+    selected_item_array[index].classList.toggle("item")
+  })
+}
 
 myButton.addEventListener("click", () => {
   if (
